28efb2d0d04e923c3513fc81b9a6f437f9036c05
Maider de Jesus <maiderdejesus@gmail.com>
2014-05-22 11:48:37 +0200

    Example of IF sentence use in Visual Basic.NET

---
+++ SentenciaIF/SentenciaIF/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.Consultar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(13, 13)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 0
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(136, 13)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 1
+        '
+        'Consultar
+        '
+        Me.Consultar.Location = New System.Drawing.Point(91, 63)
+        Me.Consultar.Name = "Consultar"
+        Me.Consultar.Size = New System.Drawing.Size(75, 23)
+        Me.Consultar.TabIndex = 2
+        Me.Consultar.Text = "Consultar"
+        Me.Consultar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Consultar)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents Consultar As System.Windows.Forms.Button
+
+End Class
---
+++ SentenciaIF/SentenciaIF/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub Consultar_Click(sender As Object, e As EventArgs) Handles Consultar.Click
+        '        Dim numero1, numero2 As Integer
+
+        '        numero1 = txtNum1.Text
+        '        numero2 = txtNum2.Text
+
+        '        If (numero1 < numero2) Then
+        '        MsgBox("Numero1 menor que Numero2")
+        '        ElseIf (numero1 = numero2) Then
+        '        MsgBox("Numero1 igual que Numero2")
+        '        Else
+        '        MsgBox("Numero1 mayor que Numero2")
+        '        End If
+
+        If (txtNum1.Text.Contains("ol")) Then
+            MsgBox("La frase contiene la cadena de caracteres OL")
+        Else
+            MsgBox("La frase no contiene la cadena de caracteres OL")
+        End If
+    End Sub
+End Class
