65f3f2c0a7b38536d3f0cea5f552d8baf1631c95
Maider de Jesus <maiderdejesus@gmail.com>
2014-05-22 12:04:51 +0200

    Example of FOR use in Visual Basic.NET

---
+++ BucleFor/BucleFor/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '        Dim i As Integer
+
+        '        For i = 0 To 4
+        '        MsgBox(i)
+        '        Next
+
+        '        Dim array(10) As Integer
+        '        Dim i As Integer
+
+        '        For i = 0 To 9
+        '        Array(i) = i + 1
+        '        MsgBox("Array(" + i.ToString + ") = " + Array(i).ToString)
+        '        Next
+
+        Dim i As Integer
+        Dim j As Integer
+
+        For i = 0 To 2
+            MsgBox("Bucle externo i = " + i.ToString)
+            For j = 0 To 3
+                MsgBox("Bucle interno j = " + j.ToString)
+            Next
+        Next
+    End Sub
+End Class
