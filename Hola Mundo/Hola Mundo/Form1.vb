4c01cf31666d3ace8dd4522d437b640bd539a951
Maider de Jesus <maiderdejesus@gmail.com>
2014-05-22 11:46:55 +0200

    Hello World in Visual Basic.NET

---
+++ Hola Mundo/Hola Mundo/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Hola Mundo del estilo AbortRetryIgnore", MsgBoxStyle.AbortRetryIgnore, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo ApplicationModal", MsgBoxStyle.ApplicationModal, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo Critical", MsgBoxStyle.Critical, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo DefaultButton1", MsgBoxStyle.DefaultButton1, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo DefaultButton2", MsgBoxStyle.DefaultButton2, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo DefaultButton3", MsgBoxStyle.DefaultButton3, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo Exclamation", MsgBoxStyle.Exclamation, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo Information", MsgBoxStyle.Information, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo MsgBoxHelp", MsgBoxStyle.MsgBoxHelp, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo MsgBoxRight", MsgBoxStyle.MsgBoxRight, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo MsgBoxRtlReading", MsgBoxStyle.MsgBoxRtlReading, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo MsgBoxSetForeground", MsgBoxStyle.MsgBoxSetForeground, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo OkCancel", MsgBoxStyle.OkCancel, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo OkOnly", MsgBoxStyle.OkOnly, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo Question", MsgBoxStyle.Question, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo RetryCancel", MsgBoxStyle.RetryCancel, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo SystemModal", MsgBoxStyle.SystemModal, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo YesNo", MsgBoxStyle.YesNo, "Hola Mundo")
+        MsgBox("Hola Mundo del estilo YesNoCancel", MsgBoxStyle.YesNoCancel, "Hola Mundo")
+    End Sub
+End Class
