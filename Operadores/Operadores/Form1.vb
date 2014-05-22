09f79ec17711a687f9db3ca78fe5486db889744f
Maider de Jesus <maiderdejesus@gmail.com>
2014-05-22 11:47:24 +0200

    Example of Operators use in Visual Basic.NET

---
+++ Operadores/Operadores/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim num1, num2, resultado As Integer
+        Dim resultado2 As Double
+        num1 = 2
+        num2 = 5
+        resultado = num1 + num2
+        MsgBox(resultado.ToString)
+
+        resultado = num1 - num2
+        MsgBox(resultado.ToString)
+
+        resultado = num1 * num2
+        MsgBox(resultado.ToString)
+
+        resultado2 = num1 / num2
+        MsgBox(resultado2.ToString)
+
+        resultado = num1 Mod num2
+        MsgBox(resultado.ToString)
+
+        resultado = 5 Mod num2
+        MsgBox(resultado.ToString)
+
+        num1 += 1 'Para incrementar en 1
+        MsgBox(num1.ToString)
+
+        num1 += 5 'Para incrementar en 5
+        MsgBox(num1.ToString)
+
+        num1 -= 5
+        MsgBox(num1.ToString)
+    End Sub
+End Class
